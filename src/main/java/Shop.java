70031f06518cd6827e709df7750f07fc704fde06
SShingler <s.shingler@hotmail.co.uk>
2023-04-21 14:45:43 +0100

    test Shop - MVP & extensions complete

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,58 @@
+import Behaviours.ISell;
+import Instruments.Instrument;
+import Instruments.Piano;
+import Miscellaneous.MiscellaneousItem;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private double till;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name, double till) {
+        this.name = name;
+        this.till = till;
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public int getStock() {
+        return stock.size();
+    }
+
+    public void addStock(ISell item){
+        this.stock.add(item);
+        if (item instanceof Instrument){
+            this.till -= ((Instrument)item).getBuyPrice();
+        } else {
+            this.till -=((MiscellaneousItem)item).getBuyPrice();
+        }
+    }
+
+    public void sellStock(ISell item) {
+        this.stock.remove(item);
+        if (item instanceof Instrument){
+            this.till += ((Instrument)item).getSellPrice();
+        } else {
+            this.till +=((MiscellaneousItem)item).getSellPrice();
+        }
+
+    }
+
+    public double getPotentialProfit() {
+        double totalPotentialProfit = 0;
+        for (ISell item : this.stock){
+            totalPotentialProfit += item.getMarkup();
+        }
+        return totalPotentialProfit;
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,68 @@
+import Instruments.Guitar;
+import Instruments.Piano;
+import Instruments.InstrumentType;
+import Miscellaneous.GuitarStrings;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+    Guitar guitar;
+    Piano piano;
+    GuitarStrings guitarStrings;
+
+
+    @Before
+    public void Setup(){
+        shop = new Shop("CodeClan Music Shop", 1000);
+        guitar = new Guitar("Mahogany", "Midnight Blue", InstrumentType.GUITAR, 380, 450, 6);
+        piano = new Piano("Maple", "Polished maple", InstrumentType.PIANO, 600, 720, "Grand Piano");
+        guitarStrings = new GuitarStrings(2.50, 5.50, "Earnie Balls", "Regular Slinky");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("CodeClan Music Shop", shop.getName());
+    }
+
+    @Test
+    public void canGetTill(){
+        assertEquals(1000, shop.getTill(), 0.01);
+    }
+
+    @Test
+    public void stockListStartsEmpty(){
+        assertEquals(0, shop.getStock());
+    }
+
+    @Test
+    public void shopCanAddStock(){
+        shop.addStock(guitar);
+        assertEquals(1, shop.getStock());
+        assertEquals(620, shop.getTill(), 0.01);
+    }
+
+    @Test
+    public void shopCanSellStock(){
+        shop.addStock(piano);
+        shop.addStock(guitar);
+        shop.addStock(guitarStrings);
+        shop.sellStock(piano);
+        shop.sellStock(guitar);
+        shop.sellStock(guitarStrings);
+        assertEquals(0, shop.getStock());
+        assertEquals(1193, shop.getTill(), 0.01);
+
+    }
+
+    @Test
+    public void canGetTotalPotentialProfit(){
+        shop.addStock(piano);
+        shop.addStock(guitar);
+        shop.addStock(guitarStrings);
+        assertEquals(193, shop.getPotentialProfit(), 0.01);
+    }
 }
