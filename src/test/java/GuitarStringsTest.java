06284a7f75e5882a914823afaab5305caa0e32a1
SShingler <s.shingler@hotmail.co.uk>
2023-04-21 14:09:48 +0100

    Test GuitarStrings & PianoTuner

--- src/test/java/GuitarStringsTest.java
+++ src/test/java/GuitarStringsTest.java
@@ -1,2 +1,39 @@
+import Miscellaneous.GuitarStrings;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarStringsTest {
+    GuitarStrings guitarStrings;
+
+    @Before
+    public void Setup() {
+        guitarStrings = new GuitarStrings(2.50, 5.50, "Earnie Balls", "Regular Slinky");
+    }
+
+    @Test
+    public void canGetBuyPrice(){
+        assertEquals(2.50, guitarStrings.getBuyPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetSellPrice(){
+        assertEquals(5.50, guitarStrings.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetDescription(){
+        assertEquals("Earnie Balls", guitarStrings.getDescription());
+    }
+
+    @Test
+    public void canGetWeight(){
+        assertEquals("Regular Slinky", guitarStrings.getWeight());
+    }
+
+    @Test
+    public void canGetMarkup(){
+        assertEquals(3, guitarStrings.getMarkup(), 0.01);
+    }
 }
--- src/test/java/PianoTunerTest.java
+++ src/test/java/PianoTunerTest.java
@@ -1,2 +1,38 @@
+import Miscellaneous.PianoTuner;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 public class PianoTunerTest {
+
+    PianoTuner pianoTuner;
+
+    @Before
+    public void Setup(){
+        pianoTuner = new PianoTuner(5.75, 12.75, "Metal tuning fork", "Aluminium");
+    }
+    @Test
+    public void canGetBuyPrice(){
+        assertEquals(5.75, pianoTuner.getBuyPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetSellPrice(){
+        assertEquals(12.75, pianoTuner.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetDescription(){
+        assertEquals("Metal tuning fork", pianoTuner.getDescription());
+    }
+
+    @Test
+    public void canGetWeight(){
+        assertEquals("Aluminium", pianoTuner.getMaterial());
+    }
+
+    @Test
+    public void canGetMarkup(){
+        assertEquals(7, pianoTuner.getMarkup(), 0.01);
+    }
 }
